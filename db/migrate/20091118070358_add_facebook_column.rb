e63c35353300074827742091cb05390f25d9edf0
Shripad K <assortmentofsorts@gmail.com>
2010-07-06 01:16:48 -0700

    removed duplicate name column entry in AddFacebookColumn. Thanks @keone.

--- db/migrate/20091118070358_add_facebook_column.rb
+++ db/migrate/20091118070358_add_facebook_column.rb
@@ -1,6 +1,5 @@
 class AddFacebookColumn < ActiveRecord::Migration
   def self.up
-    add_column :users, :name, :string
     add_column :users, :facebook_username, :string
     add_column :users, :facebook_uid, :integer, :limit => 8
     add_column :users, :birthday, :string
@@ -16,6 +15,5 @@ class AddFacebookColumn < ActiveRecord::Migration
     remove_column :users, :birthday
     remove_column :users, :facebook_username
     remove_column :users, :facebook_uid
-    remove_column :users, :name
   end
 end
